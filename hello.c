6a17f27902dfb0e3bfb038c31e49a2fd35fd304c
Evan <ezhang6@huskers.unl.edu>
2026-08-27 11:11:14 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Evan Zhang
+ * Date: 2026/08/27
+ *
+ * A program that prints my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Evan Zhang\n");
+
+  return 0;
+}
